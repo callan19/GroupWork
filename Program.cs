ea9537a883e78333e55213d1c7e1289198c58371
aidangoldie7 <40402670@live.napier.ac.uk>
2021-03-10 16:25:49 +0000

    Commit 3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Aidan, i love minecraft xx");
+            Console.WriteLine("Hello Aidan, i love warzone");
             Console.WriteLine("Hello callan again!!");
         }
     }
