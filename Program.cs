b930c49bb3bd6e638664507858b3efac21a85997
Callan <40444689@live.napier.ac.uk>
2021-04-22 16:53:42 +0100

    beginnig of vali

--- Program.cs
+++ Program.cs
@@ -6,8 +6,65 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Aidan, i love warzone");
-            Console.WriteLine("Hello callan again!!");
+          string DOB;
+          string course;
+          string firstName;
+          string lastName;
+          string gender;
+          string gradYear;
+          string instatutionName;
+          int studentID;
+
+
+        if (string.IsNullOrEmpty(DOB))
+        {
+            Console.WriteLine("Enter your date of birth");
+
+        }
+
+         if (string.IsNullOrEmpty(course))
+        {
+            Console.WriteLine("Enter the name of your course");
+
+        }
+
+         if (string.IsNullOrEmpty(firstName))
+        {
+            Console.WriteLine("Enter your first name");
+
+        }
+
+         if (string.IsNullOrEmpty(lastName))
+        {
+            Console.WriteLine("Enter your last name");
+
+        }
+
+         if (string.IsNullOrEmpty(gender))
+        {
+            Console.WriteLine("Enter your gender");
+
+        }
+
+         if (string.IsNullOrEmpty(gradYear))
+        {
+            Console.WriteLine("Enter your graduation vibe");
+
+        }
+
+         if (string.IsNullOrEmpty(instatutionName))
+        {
+            Console.WriteLine("Enter the name of your instatution");
+
+        }
+
+         if (string.IsNullOrEmpty(studentID))
+        {
+            Console.WriteLine("Enter your student ID");
+
+        }
+
+
         }
     }
 }
